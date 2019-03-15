8174a22c961e30dceed13cd711d22f90436a82c0
Guilherme Carvalho <guilhermescarvalho@protonmail.com>
2019-03-15 18:42:29 -0300

    Clean code

--- src/resource/email_resource.py
+++ src/resource/email_resource.py
@@ -1,10 +1,6 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import getpass
-from src import util
 from src.util import tables
-from src.service.sign_in import SignIn
+from src.service.email_service import SendEmail
 
 
 class Email:
@@ -13,40 +9,14 @@ class Email:
         table = tables.set_table()
 
         sql = ('SELECT * FROM %s ' % table)
-        query = util.connection.select_data(sql)
-
-        my_email = SignIn.getEmail()
-        print("Your email: " + '\033[32m' + my_email + '\033[0;0m')
 
         password = getpass.getpass(prompt='[*]Type your email password: ')
         subject = str(input('[*]Type the subject: '))
         message = str(input('[*]Write your message: '))
 
-        print('\n[>] - Emails to be sent: %i' % len(query))
+        send_email = SendEmail(password, sql, subject, message)
 
-        msg = MIMEMultipart()
+        print('\n[>] - Emails to be sent: %i' % send_email.get_emails())
 
-        number_of_sent = 0
-        for receiver in query:
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login(my_email, password)
-
-            msg['from'] = my_email
-            msg['To'] = receiver[3]
-            msg['Subject'] = subject
-            msg.attach(MIMEText(message, 'plain'))
-
-            text = msg.as_string()
-
-            server.sendmail(my_email, receiver[3], text)
-
-            number_of_sent += 1
-
-            print('[%i] of' % number_of_sent, len(query), 'sent to ' + '\033[32m' +
-                  '%s %s --> %s' % (receiver[1], receiver[2], receiver[3]) + '\033[0;0m')
-
-            server.quit()
-
-        print('[!] %i emails sent!' % len(query))
+        print('[!] %i emails sent!' % send_email.get_emails())
         print('[!] Finished')
---
+++ src/service/email_service.py
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from src.service.sign_in import SignIn
+from src import util
+
+
+class SendEmail:
+
+    def __init__(self, password, sql, subject, message):
+        query = util.connection.select_data(sql)
+        self.emails_to_send = len(query)
+
+        my_email = SignIn.getEmail()
+        print("Your email: " + '\033[32m' + my_email + '\033[0;0m')
+
+        msg = MIMEMultipart()
+
+        number_of_sent = 0
+        for receiver in query:
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(my_email, password)
+
+            msg['from'] = my_email
+            msg['To'] = receiver[3]
+            msg['Subject'] = subject
+            msg.attach(MIMEText(message, 'plain'))
+
+            text = msg.as_string()
+
+            server.sendmail(my_email, receiver[3], text)
+
+            number_of_sent += 1
+
+            print('[%i] of' % number_of_sent, len(query), 'sent to ' + '\033[32m' +
+                  '%s %s --> %s' % (receiver[1], receiver[2], receiver[3]) + '\033[0;0m')
+
+            server.quit()
+
+    def get_emails(self):
+        return self.emails_to_send
