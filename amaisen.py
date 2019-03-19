7514d5c640e6298efd100fa4d8089fcca6701efd
Guilherme Carvalho <guilhermescarvalho@protonmail.com>
2019-03-18 23:33:33 -0300

    improvements

--- amaisen.py
+++ amaisen.py
@@ -4,19 +4,14 @@ from src.resource.admin import Admin
 from src.resource.common import Common
 from src.service.init import Init
 
+while True:
 
-def start():
-    while True:
+    Init()
 
-        Init()
+    sign_in = SignIn()
+    sign_in.sign_in()
 
-        sign_in = SignIn()
-        sign_in.sign_in()
-
-        if sign_in.get_user_type() == 'admin':
-            Admin.exec_admin()
-        else:
-            Common.exec_common()
-
-
-start()
+    if sign_in.get_user_type() == 'admin':
+        Admin.exec_admin()
+    else:
+        Common.exec_common()
