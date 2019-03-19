6670e06051a8ddd4489832e330bd8508dc37441d
Guilherme Carvalho <guilhermescarvalho@protonmail.com>
2019-03-18 23:36:52 -0300

    improvements

--- amaisen.py
+++ amaisen.py
@@ -8,10 +8,9 @@ while True:
 
     Init()
 
-    sign_in = SignIn()
-    sign_in.sign_in()
+    SignIn.sign_in()
 
-    if sign_in.get_user_type() == 'admin':
+    if SignIn.get_user_type() == 'admin':
         Admin.exec_admin()
     else:
         Common.exec_common()
